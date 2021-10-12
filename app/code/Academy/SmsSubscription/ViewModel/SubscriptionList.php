d9c0562c55678490fbdedb0db43c742b7a774a04
agnnow1 <agn.now.95@gmail.com>
2021-10-12 14:18:46 +0200

    Added option to opt in and opt out from Sms Subscriptions.

--- app/code/Academy/SmsSubscription/ViewModel/SubscriptionList.php
+++ app/code/Academy/SmsSubscription/ViewModel/SubscriptionList.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 namespace Academy\SmsSubscription\ViewModel;
 
 use Academy\SmsSubscription\Model\ResourceModel\Subscription\CollectionFactory;
+use Academy\SmsSubscription\Model\Subscription;
 use Magento\Customer\Model\Session;
 use Magento\Framework\App\RequestInterface;
+use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Framework\View\LayoutInterface;
 use Magento\Theme\Block\Html\Pager;
@@ -17,17 +19,20 @@ class SubscriptionList implements ArgumentInterface
     protected LayoutInterface $layoutInterface;
     protected RequestInterface $request;
     protected ?string $pager = null;
+    protected UrlInterface $url;
 
     public function __construct(
         CollectionFactory $collectionFactory,
         Session $customerSession,
         LayoutInterface $layoutInterface,
-        RequestInterface $request
+        RequestInterface $request,
+        UrlInterface $url
     ) {
         $this->collectionFactory = $collectionFactory;
         $this->customerSession = $customerSession;
         $this->layoutInterface = $layoutInterface;
         $this->request = $request;
+        $this->url = $url;
     }
 
     public function getSubscriptions(): \Academy\SmsSubscription\Model\ResourceModel\Subscription\Collection
@@ -59,5 +64,20 @@ class SubscriptionList implements ArgumentInterface
         }
 
         return $this->pager;
+    }
+
+    public function getFormAction(): string
+    {
+        return $this->url->getUrl('sms_subscription/index/save');
+    }
+
+    public function getUpdateUrl(): string
+    {
+        return $this->url->getUrl('sms_subscription/index/update');
+    }
+
+    public function getIsSubscribed(Subscription $subscription): bool
+    {
+        return (int)$subscription->getStatus() === Subscription::STATUS_SUBSCRIBED;
     }
 }
